580f28f5fe86c6b1bae0a3fabd4c84b6cb8379e7
blackarea <supersute142@gmail.com>
2022-10-02 23:49:05 +0900

    BOJ_1547 - 공

---
+++ BOJ/BOJ_1547.java
@@ -0,0 +1,29 @@
+package algorithm.BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1547 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int M = Integer.parseInt(br.readLine());
+        int num = 1;
+
+        for(int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x =Integer.parseInt(st.nextToken());
+            int y =Integer.parseInt(st.nextToken());
+
+            if(x == num) {
+                num = y;
+            }else if(y == num){
+                num = x;
+            }
+        }
+
+        System.out.println(num);
+    }
+}
