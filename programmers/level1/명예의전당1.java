e44cc89e4c08756cd00d3ec3a48af9f75520339b
blackarea <supersute142@gmail.com>
2023-07-05 00:11:13 +0900

    프로그래머스 - leve1_명예의전당(1) - 코드 수정

--- programmers/level1/명예의전당1.java
+++ programmers/level1/명예의전당1.java
@@ -1,6 +1,7 @@
 package algorithm.programmers.level1;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class 명예의전당1 {
     public static void main(String[] args) {
@@ -10,18 +11,15 @@ public class 명예의전당1 {
 
     public static int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
-        int[] hallOfFame = new int[k];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
+        //우선순위 큐를 이용하면 가독성 좋은 코드 작성 가능
         for (int i = 0; i < score.length; i++) {
-            if(i < k){
-                hallOfFame[0] = score[i];
-            }else {
-                if(hallOfFame[0] < score[i]){
-                    hallOfFame[0] = score[i];
-                }
+            pq.add(score[i]);
+            if(pq.size() > k){
+                pq.poll();
             }
-            Arrays.sort(hallOfFame);
-            answer[i] = hallOfFame[i < k ? k - i - 1 : 0];
+            answer[i] = pq.peek();
         }
         System.out.println(Arrays.toString(answer));
 
