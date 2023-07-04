9e02da6832da5e65b778ba868806cb01af9a189a
blackarea <supersute142@gmail.com>
2023-07-04 23:59:58 +0900

    프로그래머스 - leve1_명예의전당(1)

---
+++ programmers/level1/명예의전당1.java
@@ -0,0 +1,30 @@
+package algorithm.programmers.level1;
+
+import java.util.Arrays;
+
+public class 명예의전당1 {
+    public static void main(String[] args) {
+
+        solution(3, new int[]{10, 100, 20, 150, 1, 100, 200});
+    }
+
+    public static int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        int[] hallOfFame = new int[k];
+
+        for (int i = 0; i < score.length; i++) {
+            if(i >= k){
+                if(hallOfFame[k - 1] < score[i]){
+                    hallOfFame[k - 1] = score[i];
+                }
+            }else {
+                hallOfFame[i] = score[i];
+            }
+            Arrays.sort(hallOfFame);
+            answer[i] = hallOfFame[i < k ? k - i - 1 : 0];
+        }
+        System.out.println(Arrays.toString(answer));
+
+        return answer;
+    }
+}
